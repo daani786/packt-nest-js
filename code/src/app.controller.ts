ecf4d7daed05e494124a6410d3d79dc85eabef70
Muhammad Adnan Masood <m.adnan.masood@gmail.com>
2026-07-13 16:20:38 +0500

    use HttpCode to set response code

--- code/src/app.controller.ts
+++ code/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, Query, Param } from '@nestjs/common';
+import { Controller, Get, Req, Res, Query, Param, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 import type { Request, Response } from 'express';
 
@@ -7,6 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @HttpCode(200)
   getHello(): string {
     return this.appService.getHello();
   }
